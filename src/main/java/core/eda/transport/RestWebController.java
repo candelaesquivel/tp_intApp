7ffa3e32ea873620c53e74c60e789795440a4b01
Agustin Stanoff <agustinstanoff98@gmail.com>
2023-04-08 12:06:07 -0300

    Changes RestWebController

--- src/main/java/core/eda/transport/RestWebController.java
+++ src/main/java/core/eda/transport/RestWebController.java
@@ -3,12 +3,59 @@ package core.eda.transport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 public class RestWebController {
   
-  @GetMapping("/")
-  public void publish(){
-    System.out.println("Entrando en El Hello World");
+  //ENDPOINT 1
+  @GetMapping("/publishMessage")
+ //@PostMapping("/publishMessage")
+  public void publishMessage(){
+    System.out.println("Publish Message");
   }
+  /*public void PublishMessage(@RequestParam(name="channel") String exchange, @RequestParam(name="message") String message) throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, IOException, TimeoutException  {
+		TransportController.getInstance().sendMessage(exchange, message);
+	}*/
+
+//ENDPOINT 2
+  @GetMapping("/bind")
+//@PostMapping("/bind")
+ public void bind(){
+   System.out.println("Bind");
+ }
+ /*public void bind(@RequestParam(name="canal") String exchange, @RequestParam(name="subscriber") String queue) throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, IOException, TimeoutException  {
+  TransportController.getInstance().bind(exchange, queue);
+}*/
+
+//ENDPOINT 3
+  @GetMapping("/broadcast")
+//@PostMapping("/broadcast")
+ public void broadcast(){
+   System.out.println("Broadcast");
+ }
+ /*public void broadcast(@RequestBody String message) throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, IOException, TimeoutException  {
+  TransportController.getInstance().broadcast(message);
+}*/
+
+//ENDPOINT 4
+  @GetMapping("/unbind")
+//@PostMapping("/unbind")
+ public void unbind(){
+   System.out.println("Unbind");
+ }
+ /*public void unbind(@RequestParam(name="channel") String exchange, @RequestParam(name="subscriber") String queue) throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, IOException, TimeoutException  {
+  TransportController.getInstance().unbind(exchange, queue);
+}*/
+
+
 }
+
+
+  
+
